1c280288208c9ef44a3f6b77881412cabeadf907
Nicolaj <nicolajhaueter72@gmail.com>
2023-05-31 12:55:16 +0200

    Created a server socket

--- Calculator/calculator_server.py
+++ Calculator/calculator_server.py
@@ -2,3 +2,7 @@ import socket
 import threading
 from calculator import Calculator
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = 'localhost'  # This is the host
+port = 8000  # Port of the host
+server_socket.bind((host, port))
